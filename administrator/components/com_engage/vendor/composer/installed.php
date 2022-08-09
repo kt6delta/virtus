eaf79fec698d23733a47850c6e930fb99626667b
JJaraSas <jejaras@correo.udistrital.edu.co>
2022-08-08 20:56:51 -0500

    Instalados comentarios Engage

---
+++ administrator/components/com_engage/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '2.x-dev',
+        'version' => '2.9999999.9999999.9999999-dev',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../../../',
+        'aliases' => array(),
+        'reference' => '4eb00c46a11d76c3ab5524bf4bf21e48d9630a10',
+        'name' => 'akeeba/engage',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'akeeba/engage' => array(
+            'pretty_version' => '2.x-dev',
+            'version' => '2.9999999.9999999.9999999-dev',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../../../',
+            'aliases' => array(),
+            'reference' => '4eb00c46a11d76c3ab5524bf4bf21e48d9630a10',
+            'dev_requirement' => false,
+        ),
+        'ezyang/htmlpurifier' => array(
+            'pretty_version' => 'v4.13.0',
+            'version' => '4.13.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ezyang/htmlpurifier',
+            'aliases' => array(),
+            'reference' => '08e27c97e4c6ed02f37c5b2b20488046c8d90d75',
+            'dev_requirement' => false,
+        ),
+    ),
+);
